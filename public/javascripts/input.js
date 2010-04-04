0ed613f41a6ee0f744b72b097d1993a2fc9ae5b3
Thomas Sommer <tom@-tombook-.(none)>
2010-04-04 23:15:15 +1000

    Finished input form and client side validations.

--- public/javascripts/input.js
+++ public/javascripts/input.js
@@ -12,9 +12,35 @@ $(function() {
     $("#error").html("");
     var project_id = $("#project-select").find("option:selected").attr("value");
     var iteration_id = $("form .iterations").find("option:selected").attr("value");
+    var hours = $("#hours").val().trim();
+    var income = $("#income").val().trim();
 
     if (project_id == "0") {
       $("#error").html("Please select a project.");
+    }
+    else if (hours.length == 0) {
+      $("#error").html("Please provide the hours worked in the selected iteration.");
+    }
+    else if (hours.match(/^\d+$/) == null) {
+      $("#error").html("Please provide the hours worked in a valid format: No decimal digits, No dollar sign.");
+    }
+    else if (income.length > 0 && income.match(/^\d+$/) == null) {
+      $("#error").html("Please provide the money earned in a valid format: No decimal digits, No dollar sign.");
+    }
+    else {
+      $.ajax({
+        type: "PUT",
+        url: "/projects/" + project_id + "/iterations/" + iteration_id,
+        data: {
+          hours: hours,
+          income: income
+        },
+        success: function() {
+          alert("Update successful!");
+          $("#hours").val("");
+          $("#income").val("");
+        }
+      });
     }
   });
 });
