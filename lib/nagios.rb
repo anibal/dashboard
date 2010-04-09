28569f351e9122c7ebe2ab5949b121bd08ff5892
Thomas Sommer <tom@-tombook-.(none)>
2010-04-09 11:27:05 +1000

    Fixed Nagios problem fetching.

--- lib/nagios.rb
+++ lib/nagios.rb
@@ -2,13 +2,13 @@ class Nagios
 
   class << self
     def status
-      doc = open(NAGIOS_URL, :http_basic_authentication => ["dashboard", "Sheinei3"]) { |f| Hpricot(f) }
+      doc = open(NAGIOS_URL, :http_basic_authentication => [NAGIOS_USER, NAGIOS_PW]) { |f| Hpricot(f) }
 
       totals, problems = doc.search("table.serviceTotals")
       {
-        :system_count => totals.at("td.serviceTotalsOK").innerHTML.to_i,
-        :problem_count => problems.at("td.serviceTotals").innerHTML.to_i,
-        :problems => doc.search("table.status tr")[1..-1].map { |problem| "#{problem.search("td")[0].innerHTML} (#{problem.search("td")[1].innerHTML})" }
+        :system_count => totals.at("td.serviceTotalsOK").inner_html.to_i,
+        :problem_count => problems.at("td.serviceTotals").inner_html.to_i,
+        :problems => doc.search("table.status > tr")[1..-1].map { |problem| "#{problem.search("> td")[1].inner_text.strip} (#{problem.search("> td")[0].inner_text.strip})" }
       }
     end
   end
