71dcca13f50eec2d183ffe8461f9704526b0a266
Thomas Sommer <tom@-tombook-.(none)>
2010-04-13 18:22:00 +1000

    Fixed nagios status fetching.

--- lib/nagios.rb
+++ lib/nagios.rb
@@ -8,7 +8,10 @@ class Nagios
       {
         :system_count => totals.at("td.serviceTotalsOK").inner_html.to_i,
         :problem_count => (problems.at("td.serviceTotalsPROBLEMS") || problems.at("td.serviceTotals")).inner_html.to_i,
-        :problems => doc.search("table.status > tr")[1..-1].map { |problem| "#{problem.search("> td")[1].inner_text.strip} (#{problem.search("> td")[0].inner_text.strip})" }
+        :problems => doc.
+                      search("table.status > tr")[1..-1].
+                      select { |problem| problem.search("> td").size > 1 }.
+                      map { |problem| "#{problem.search("> td")[1].inner_text.strip} (#{problem.search("> td")[0].inner_text.strip})" }
       }
     end
   end
