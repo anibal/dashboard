2ac2a9599dc56ac214d11536c5429b161cfe9a16
David Goodlad <david@goodlad.ca>
2010-05-06 17:59:52 +1000

    Grab time entries in groups of 24 hours

--- script/update_slimtimer.rb
+++ script/update_slimtimer.rb
@@ -16,30 +16,18 @@ st = SlimtimerApi.new(config[:api_key].to_s,
 
 puts "Slimtimer connected as user id #{st.user_id}"
 
+puts "Loading tasks"
 tasks = st.tasks
+puts "  Got #{tasks.size} tasks; updating"
 SlimtimerTask.update(tasks)
 
-#last_entry = TimeEntry.first(:order => [:end_time.desc])
-last_entry = nil
-start_range = last_entry ? last_entry.end_time : Time.local(2010, 5, 1)
-end_range = start_range + 24 * 60 * 60
-
-puts "Loading time entries from #{start_range} to #{end_range}"
-
-entries = st.time_entries(start_range.strftime(FULL_DATE_TIME),
-                          end_range.strftime(FULL_DATE_TIME))
-
-puts "Got #{entries.size} entries"
-
-puts "Loading user from db"
-
+puts "Loading db user"
 u = SlimtimerUser.get(st.user_id)
 if u.nil?
   puts "  Building new user"
   u = SlimtimerUser.new
   u.id = st.user_id
-  entry = entries.first
-  task = entry['task']
+  task = tasks.first # FIXME Assumes that at least one task was returned
   person = %w[owners coworkers reporters].map { |k|
     a = if task[k].nil? || task[k]['person'].nil?
       []
@@ -54,7 +42,7 @@ if u.nil?
     a
   }.flatten.find { |person| person['user_id'] == u.id }
 
-  puts "    Found person in entries:"
+  puts "    Found person in one of their tasks"
   pp person
 
   u.name = person['name']
@@ -62,6 +50,23 @@ if u.nil?
   u.save
 end
 
-puts "Updating time entries"
-u.update_time_entries(entries)
+#last_entry = TimeEntry.first(:order => [:end_time.desc])
+last_entry = nil
+start_range = last_entry ? last_entry.end_time : Time.local(2010, 5, 1)
+end_range = start_range + 24 * 60 * 60
 
+until end_range >= Time.now
+  puts "Loading time entries from #{start_range} to #{end_range}"
+
+  entries = st.time_entries(start_range.strftime(FULL_DATE_TIME),
+                            end_range.strftime(FULL_DATE_TIME))
+
+  puts "Got #{entries.size} entries"
+
+
+  puts "Updating time entries"
+  u.update_time_entries(entries)
+
+  start_range = end_range
+  end_range = start_range + 24 * 60 * 60
+end
