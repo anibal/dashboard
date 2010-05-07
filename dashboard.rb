84901e57e12d860e7c7d1293cfe7f15ba8d7f16f
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2010-05-07 15:37:03 +1000

    config belongs in root or config, not lib

--- dashboard.rb
+++ dashboard.rb
@@ -1,7 +1,7 @@
 %w[date rubygems sinatra sinatra/content_for haml dm-core dm-aggregates open-uri hpricot json librmpd yahoo-weather].each { |lib| require lib }
 %w[ext/fixnum mpd_proxy ci pivotal pivotal_api nagios].each { |lib| require "lib/#{lib}" }
 %w[iteration slimtimer_task slimtimer_user time_entry time_report].each { |model| require "models/#{model}" }
-require 'lib/config'
+require 'config'
 
 # -----------------------------------------------------------------------------------
 # Helpers
