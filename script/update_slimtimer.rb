9ae759344b1247d181a35566044c1ee2c90caf23
David Goodlad <david@goodlad.ca>
2010-05-13 10:48:17 +1000

    Minor logging changes in slimtimer update script

--- script/update_slimtimer.rb
+++ script/update_slimtimer.rb
@@ -11,22 +11,20 @@ require 'pp'
 
 FULL_DATE_TIME = "%F %T"
 
-puts "Loading slimtimer"
-
 SLIMTIMER_USERS.each do |email, password|
+  puts "Loading slimtimer data for #{email}"
   st = SlimtimerApi.new(SLIMTIMER_APIKEY, email, password)
+  puts "  Slimtimer connected as user id #{st.user_id}"
 
-  puts "Slimtimer connected as user id #{st.user_id}"
-
-  puts "Loading tasks"
+  puts "  Loading tasks"
   tasks = st.tasks
-  puts "  Got #{tasks.size} tasks; updating"
+  puts "    Got #{tasks.size} tasks; updating"
   SlimtimerTask.update(tasks)
 
-  puts "Loading db user"
+  puts "  Loading db user"
   u = SlimtimerUser.get(st.user_id)
   if u.nil?
-    puts "  Building new user"
+    puts "    Building new user"
     u = SlimtimerUser.new
     u.id = st.user_id
     task = tasks.first # FIXME Assumes that at least one task was returned
@@ -44,8 +42,8 @@ SLIMTIMER_USERS.each do |email, password|
       a
     }.flatten.find { |person| person['user_id'] == u.id }
 
-    puts "    Found person in one of their tasks"
-    pp person
+    puts "      Found person in one of their tasks:"
+    puts "      '#{person['name']}'"
 
     u.name = person['name']
     u.email = person['email']
@@ -53,19 +51,19 @@ SLIMTIMER_USERS.each do |email, password|
   end
 
   last_entry = u.time_entries.first(:order => [:end_time.desc])
-  start_range = last_entry ? last_entry.end_time : Time.local(2010, 5, 1)
+  start_range = last_entry ? last_entry.end_time : Time.local(2010, 4, 1)
   end_range = [start_range + 24 * 60 * 60, Time.now].min
 
   until end_range >= Time.now
-    puts "Loading time entries from #{start_range} to #{end_range}"
+    puts "  Loading time entries from #{start_range} to #{end_range}"
 
     entries = st.time_entries(start_range.strftime(FULL_DATE_TIME),
                               end_range.strftime(FULL_DATE_TIME))
 
-    puts "Got #{entries.size} entries"
+    puts "    Got #{entries.size} entries"
 
 
-    puts "Updating time entries"
+    puts "    Updating time entries"
     u.update_time_entries(entries)
 
     start_range = end_range
