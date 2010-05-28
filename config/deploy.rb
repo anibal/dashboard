e6c75ca359aec9ce03b0c5da0662a2fdb139b145
Peter Hollows <peter.hollows@gmail.com>
2010-05-28 11:23:21 +1000

    Capify project.

---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+trikelibs = Dir['config/cap-tasks/*.rb'].reject{ |file| file =~ /(radiant)/ }
+trikelibs.each { |trikelib| load(trikelib)  }
+
+set :application, "dashboard"
+set :repository,  "git://github.com/tricycle/dashboard.git"
+
+set :scm, :git
+set :repository_cache, 'cached-copy'
+set :git_enable_submodules, 1
+set :deploy_via, :remote_cache
+
+role :web, "pepper.trike.com.au"                          # Your HTTP server, Apache/etc
+role :app, "pepper.trike.com.au"                          # This may be the same as your `Web` server
+role :db,  "pepper.trike.com.au", :primary => true # This is where Rails migrations will run
+
+set :user, "www-data"
+set :engine, "passenger"
+
+set :deploy_to, "/srv/www/#{application}"
+
+after "deploy:update_code", "deploy:update_app_config"
+
+namespace :deploy do
+  task :update_app_config do
+    run "ln -sf #{shared_path}/config.rb #{release_path}/config.rb"
+  end
+end
+
+#namespace :deploy do
+#  task :restart do
+#    run "touch #{current
+#    sudo "/etc/init.d/apache2 restart"
+#  end
+#end
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
