d132ed5c7856c94f60bbc5f28e888c2f3d7a8c17
Thomas Sommer <tom@-tombook-.(none)>
2010-06-08 18:08:44 +1000

    Small changes to the nagios status.

--- public/javascripts/nagios.js
+++ public/javascripts/nagios.js
@@ -1,7 +1,6 @@
 function fetchNagiosStatus() {
   $.getJSON("/nagios_status", function(data) {
-    $("#nagios-status .system-count").html(data.system_count);
-    $("#nagios-status .problem-count").html(data.problem_count);
+    $("#nagios-status .points").html(data.problem_count + "/" + data.system_count);
     $("#nagios-status .problems").html(data.problems.join("<br />"));
 
     if (_(data.problems).isEmpty()) {
