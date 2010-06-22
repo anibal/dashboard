576bd55a453b374ffb201eab0f5cdb918083f506
Thomas Sommer <tom@-tombook-.(none)>
2010-06-22 16:51:47 +1000

    Fixed the slimtimer code, when we can't find any tasks.

--- lib/slimtimer.rb
+++ lib/slimtimer.rb
@@ -2,9 +2,9 @@ class Slimtimer
 
   class << self
     def status_for(status, iteration_dates)
-      if iteration_dates
-        tasks = status[:ids].inject([]) { |set, id| set | SlimtimerTask.all(:name.like => "%:#{id} %") }
+      tasks = status[:ids].inject([]) { |set, id| set | SlimtimerTask.all(:name.like => "%:#{id} %") }
 
+      if iteration_dates && !tasks.empty?
         iteration_seconds = []
         iteration_dates.each do |dates|
           iteration_seconds << tasks.time_entries.all(:start_time.gt => dates[0], :start_time.lt => dates[1]).sum(:duration_in_seconds)
