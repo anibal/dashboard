a5af510c20d8e98d77ab8df5a12cee39786f1eb3
Peter Hollows <peter.hollows@gmail.com>
2010-09-22 10:15:22 +1000

    Refactor problems...

--- lib/nagios.rb
+++ lib/nagios.rb
@@ -7,15 +7,14 @@ class Nagios
       problems = doc.
         search("table.status > tr")[1..-1].
         select { |problem| problem.search("> td").size > 1 }.
-        select { |problem| problem.search("img[@src $= 'ack.gif']").size == 0 && problem.search("img[@src $= 'ndisabled.gif']").size == 0 }
+        select { |problem| problem.search("img[@src $= 'ack.gif']").size == 0 && problem.search("img[@src $= 'ndisabled.gif']").size == 0 }.
+        map    { |problem| "#{problem.search("> td")[1].inner_text.strip} (#{problem.search("> td")[0].inner_text.strip})" }
 
       {
         :system_count => doc.search("table.serviceTotals").at("td.serviceTotalsOK").inner_html.to_i,
         :problem_count => problems.size,
         :critical_count => problems.grep(/^C:/).size,
-        :problems => problems.map { |problem|
-          "#{problem.search("> td")[1].inner_text.strip} (#{problem.search("> td")[0].inner_text.strip})"
-        }
+        :problems => problems
       }
     end
   end
