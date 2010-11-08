13611a37fb1f5283de0d09a2d4ed312b09ef5f74
Tom <tom@trike.com.au>
2010-11-08 17:58:13 +1100

    Map hudson colors to dashboard colors.

--- lib/ci.rb
+++ lib/ci.rb
@@ -9,9 +9,22 @@ class CI
       unless project = jobs.find { |p| p["name"] == name }
         status[:status] = "no_ci"
       else
-        status[:status] = project["color"]
+        status[:status] = color_map(project["color"])
         status[:health] = health(project["healthReport"])
       end
+    end
+
+  private
+
+    def color_map(ci_color)
+      return case
+        when %w[green blue].include?(ci_color)
+          "green"
+        when ci_color.include?("anime")
+          "yellow"
+        else
+          "red"
+        end
     end
 
     def health(report)
