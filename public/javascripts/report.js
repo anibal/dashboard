569e195d13e343b51c576a413927f800897579c7
Peter Hollows <peter.hollows@gmail.com>
2011-04-23 12:22:05 +1000

    Make header sticky on reports

--- public/javascripts/report.js
+++ public/javascripts/report.js
@@ -14,4 +14,17 @@ $(function() {
       type:   'POST'
     });
   });
+
+  // Fix widths of floating headers in report table.
+  // Floating header cell widths do not match table column widths because the
+  // CSS positioning ejects it from the table layout.  We double-render the
+  // header, once hidden as a filler to provide reference widths and a space
+  // for the floating header to sit at the top of the table.
+  var rowWidth = 0;
+  $.each($("#timereport thead tr.header th"), function(index, cell) {
+    var refCell = $("#timereport thead tr.filler th:eq(" + index + ")");
+    $(cell).css('width', refCell.width());
+    rowWidth += refCell.outerWidth();
+  });
+  $("#timereport thead tr.header").css('width', rowWidth);
 });
