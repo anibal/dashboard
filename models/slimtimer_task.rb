1af4d32077527cdcf333ffab4b85cb4a8f225eb5
Peter Hollows <peter.hollows@gmail.com>
2011-04-27 14:11:55 +1000

    Add SlimtimerTask#canonical_name

--- models/slimtimer_task.rb
+++ models/slimtimer_task.rb
@@ -12,6 +12,18 @@ class SlimtimerTask
     name =~ /^s:/
   end
 
+  # The most-recent canonical format of this task name.
+  # e.g. Time clocked against "t:jas docs" is now clocked against "i:jas docs",
+  # so "i:jas" is now the canonical form of "t:jas".
+  def canonical_name
+    cname = name.dup
+
+    # make sure old i-class tasks now start with i
+    cname.gsub! /^t:(dma|jas|kud|pbk|plan|pod|rad) (.*)$/, 'i:\1 \2'
+
+    cname
+  end
+
   def self.update(tasks)
     tasks.each do |task_attributes|
       task = get(task_attributes['id']) || new
